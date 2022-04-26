9d90984b287145cb413b5effb9d62c5bfd1551e9
manhir <53478716+manhir@users.noreply.github.com>
2022-04-26 16:23:19 +0300

    upd seo

--- src/components/PageLayout/index.tsx
+++ src/components/PageLayout/index.tsx
@@ -1,24 +1,38 @@
 import { Header } from 'src/components/Header'
 import { useState, useCallback } from 'react'
 import { Menu } from '../Menu'
+import { NextSeo } from 'next-seo'
 
-export const PageLayout: React.FC<any> = props => {    
+export const PageLayout: React.FC<any> = props => {
     const [isOpen, setIsOpen] = useState(false)
     const onClickMenu = useCallback(() => setIsOpen(!isOpen), [isOpen])
-    const onClick = useCallback((i:number) => {
+    const onClick = useCallback((i: number) => {
         setIsOpen(false)
         if (i == 2) {
             props.openModal()
         }
-    },[])
+    }, [])
 
     return (
         <main>
+            <NextSeo
+                title='Исследование Ульяновска и Ульяновско-Димитровградской агломерации'
+                description='Приветствуем вас на сайте, посвящённом разработке комплексного аналитического исследования Ульяновска и Ульяновско-Димитровградской агломерации.'
+                openGraph={{
+                    images: [
+                        {
+                            url: '/static/hero.jpg',
+                            width: 1200,
+                            height: 717,
+                        }
+                    ]
+                }}
+            />
             <Header
                 isOpen={isOpen}
                 onClickMenu={onClickMenu}
                 menu={(
-                    <Menu 
+                    <Menu
                         onClick={onClick}
                     />
                 )}
