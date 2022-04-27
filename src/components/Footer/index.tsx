d08b7f6a29b1f333346a44b9be72e5615fa7b594
manhir <53478716+manhir@users.noreply.github.com>
2022-04-27 16:50:09 +0300

    upd footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,10 @@
 import s from './styles.module.css'
 import { Button } from '../Button'
 import Image from 'next/image'
+import oblast from '/public/static/Герб_Ульяновской_области_(2013).svg'
+import urbanika from '/public/static/urbanika.svg'
+import centr from '/public/static/centr.svg'
+import unit from '/public/static/unit.jpg'
 
 export const Footer: React.SFC = props => {
 
@@ -8,52 +12,57 @@ export const Footer: React.SFC = props => {
         <footer className={s.footer}>
             <div className={s.items}>
                 <div className={s.item}>
-                    <a href='https://minstroyrf.gov.ru/'>
-                        <Image
-                            src='/static/minstroy.jpg'
-                            width={120}
-                            height={90}
-                        />
-                    </a>
+                    <Image
+                        src={oblast}
+                        objectFit='fill'
+                    />
                     <div className={s.text}>
-                        Минстрой России
+                        Губернатор и Правительство<br />
+                        Ульяновской области
                     </div>
                 </div>
-
                 <div className={s.item}>
-                    <a href='https://www.v-tura.ru/'>
-                        <Image
-                            src='/static/admintura.jpg'
-                            width={65}
-                            height={81}
-                        />
-                    </a>
+                    <Image
+                        src={oblast}
+                        objectFit='fill'
+                    />
                     <div className={s.text}>
-                        Администрация города<br/>
-                        Верхняя Тура
+                        Центр стратегических исследований<br />
+                        Ульяновской области
                     </div>
                 </div>
-
                 <div className={s.item}>
-                    <a href="https://unit4.io" 
-                        style={{
-                            maxWidth: '200px'
-                        }}
-                    >
-                        <Image
-                            src='/static/unit.jpg'
-                            width={1590}
-                            height={307}
-                        /> 
-                    </a>
+                    <Image
+                        src={urbanika}
+                        objectFit='fill'
+                    />
                     <div className={s.text}>
-                        Проектная студия<br/>
-                        дизайн&nbsp;юнит&nbsp;4
+                        Институт территориального планирования
+                    </div>
+                </div>
+                <div className={s.item}>
+                    <Image
+                        src={centr}
+                        objectFit='fill'
+                    />
+                    <div className={s.text}>
+                        «Центр стратегических разработок<br />
+                         «Северо-Запад»
+                    </div>
+                </div>
+                <div className={s.item}>
+                    <Image
+                        src={unit}
+                        objectFit='fill'
+                    />
+                    <div className={s.text}>
+                        Проектная студия<br />
+                        дизайн юнит 4
                     </div>
                 </div>
             </div>
 
-            <hr />
+            {/* <hr />
             <div className={`${s.text}`}>
                 <Button
                     href={'https://unit4.io'}
@@ -61,7 +70,7 @@ export const Footer: React.SFC = props => {
                 >
                     ©2022 design : : unit 4
                 </Button>
-            </div>
+            </div> */}
         </footer>
     )
 }
